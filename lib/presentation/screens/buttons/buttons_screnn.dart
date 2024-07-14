fede670511072d0c53d8e28c3beda881fc546954
Rabindranath Ferreira <ferreiralox@gmail.com>
2024-07-14 20:01:07 +0200

    implements custom button and screen for check many btns

--- lib/presentation/screens/buttons/buttons_screnn.dart
+++ lib/presentation/screens/buttons/buttons_screnn.dart
@@ -77,8 +77,33 @@ class _ButtonsView extends StatelessWidget {
               style: ButtonStyle(
                   backgroundColor: WidgetStatePropertyAll(colors.primary),
                   iconColor: const WidgetStatePropertyAll(Colors.white)),
-            )
+            ),
+
+            // TODO: Custom Btn
+            const CustomButton(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Custom BTN'),
+          ),
         ),
       ),
     );
