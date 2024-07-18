dea9eee5915902bc76bcfd12f7ac1cf74e8cc8e1
Rabindranath Ferreira <rabindranath.ferreira@ingka.com>
2024-07-18 17:33:48 +0200

    implement tutorial and infinite scroll screen

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,8 +1,11 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class InfiniteScrollScreen extends StatefulWidget {
-  static const String name = 'infite_screen';
+
+  static const name = 'infinite_screen';
+
   const InfiniteScrollScreen({super.key});
 
   @override
@@ -10,7 +13,82 @@ class InfiniteScrollScreen extends StatefulWidget {
 }
 
 class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
-  List <int> imagesIds = [1,2,3,4,5];
+
+  List<int> imagesIds = [1,2,3,4,5];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+        if( (scrollController.position.pixels + 500) >= scrollController.position.maxScrollExtent ) {
+          // Load next page
+          loadNextPage();
+        }
+    });
+
+
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  Future loadNextPage() async {
+
+    if ( isLoading ) return;
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    addFiveImages();
+    isLoading = false;
+
+    if( !mounted ) return;
+
+    setState(() {});
+    moveScrollToBottom();
+  }
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 3));
+    if ( !mounted ) return;
+
+    isLoading = false;
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1 );
+    addFiveImages();
+
+    setState(() {});
+  }
+
+
+  void moveScrollToBottom() {
+    if( scrollController.position.pixels + 100 <= scrollController.position.maxScrollExtent ) return;
+
+    scrollController.animateTo(
+      scrollController.position.pixels + 120,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.fastOutSlowIn
+    );
+  }
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+
+    imagesIds.addAll(
+      [1,2,3,4,5].map((e) => lastId + e)
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,23 +97,35 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
       body: MediaQuery.removePadding(
         context: context,
         removeTop: true,
-        child: ListView.builder(
-          itemCount: imagesIds.length,
-          itemBuilder: (context, index) {
-          return FadeInImage(
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: 300,
-            placeholder: const AssetImage('assets/images/jar-loading.gif'),
-            image: NetworkImage('https://picsum.photos/id/${ imagesIds[index] }/500/300/'),
-          );
-
-        }),
+        removeBottom: true,
+        child: RefreshIndicator(
+          onRefresh: onRefresh,
+          edgeOffset: 10,
+          strokeWidth: 2,
+          child: ListView.builder(
+            controller: scrollController,
+            itemCount: imagesIds.length,
+            itemBuilder: (context, index) {
+              return FadeInImage(
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 300,
+                placeholder: const AssetImage('assets/images/jar-loading.gif'), 
+                image: NetworkImage('https://picsum.photos/id/${ imagesIds[index] }/500/300'),
+              );
+            },
+          ),
+        ),
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () => context.pop(),
-        child: const Icon(Icons.arrow_back)
+        onPressed: ()=> context.pop(),
+        child: isLoading
+          ? SpinPerfect(
+              infinite: true,
+              child: const Icon(Icons.refresh_rounded)
+            )
+          : FadeIn(child: const Icon(Icons.arrow_back_ios_new_outlined)),
       ),
     );
   }
